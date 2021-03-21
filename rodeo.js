59faf7263c36d414a9ad39243cb95cf276f7bc20
Ian Hunter <ianhunter@gmail.com>
2021-03-21 10:55:34 -0500

    initial

---
+++ rodeo.js
@@ -0,0 +1,15 @@
+
+module.exports = function (config) {
+  config.addPassthroughCopy({ "site/img/*": "img" })
+
+  return {
+    ...config,
+    dir: {
+      input: "site",
+      data: "../data",
+      includes: "includes",
+      output: "dist",
+    },
+    templateFormats: ["html", "hbs", "md"],
+  }
+}
